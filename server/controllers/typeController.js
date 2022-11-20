5c0d942c17974e61e58dd5a3b1e254309668a6d5
Kirllik <krejg007@rambler.ru>
2022-11-21 01:49:08 +0300

    Методы create и getAll в TypeController реализующие POST, GET в типах товара

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,16 +1,22 @@
+const {Type} = require('../models/models') //Деструктурированный импорт из моделей
+const ApiEror = require('../error/ApiEror')
+
 class TypeController {
 
-    async get(reg, res) {
-
-    }
-
     async create(reg, res) {
-
+        const {name} = reg.body  //Деструкт. из body запроса reg -> значения name
+        const type = await Type.create({name}) //В переменную type ложим await функцию параметром передаём name
+        return res.json(type) //return Тормозит функцию и парсит в ответе
     }
 
     // async delete(reg, res) {
     //
     // }
+
+    async getAll(reg, res) {
+        const types = await Type.findAll()
+        return res.json(types)
+    }
 }
 
 module.exports = new TypeController()
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,7 +1,9 @@
 const Router = require('express') //получаем Router из express
-const router= new Router()   //создаём объект полученого Routera
+const router = new Router()   //создаём объект полученого Routera
+const typeController = require('../controllers/typeController')
 
-router.post('/',)
-router.get('/',)
+router.post('/', typeController.create)
+router.get('/', typeController.getAll)
+// router.delete('/', typeController.delete)
 
 module.exports = router
