dd2effb065d5ed8e8661889b590bc13f8adbed63
Kirllik <krejg007@rambler.ru>
2022-12-03 01:55:45 +0300

    Create Directories and Pages in components

---
+++ client/src/components/Bascket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bascket = () => {
+    return (
+        <div>
+            BASKET
+        </div>
+    );
+};
+
+export default Bascket;
---
+++ client/src/components/deviceShop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeviceShop = () => {
+    return (
+        <div>
+          DEVICE PAGE
+        </div>
+    );
+};
+
+export default DeviceShop;
