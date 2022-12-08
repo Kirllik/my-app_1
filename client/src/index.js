a2d6b73402e984acae8f133a30bec0f20dcfd3c4
Kirllik <krejg007@rambler.ru>
2022-12-09 01:57:31 +0300

    Создание DeviceStore и проброс его в App

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
 import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
 export const Context = createContext(null) //Создание Contextа
 
@@ -11,7 +12,8 @@ export const Context = createContext(null) //Создание Contextа
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Context.Provider value={{    /*Проброс контекста в приложение */
-        user: new UserStore()     /*прокидываем объекта стора*/
+        user: new UserStore(),     /*прокидываем объекта стора*/
+        device: new DeviceStore()
     }}>
         <DevSupport ComponentPreviews={ComponentPreviews}
                     useInitialHook={useInitial}
---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,69 @@
+import {makeAutoObservable} from "mobx";
+
+export default class DeviceStore {
+    constructor() {
+        this._types = [
+            {id: 1, name: "Масла"},
+            {id: 2, name: "Шины"}
+        ]
+        this._brands = [
+            {id: 1, name: "Nokian"},
+            {id: 1, name: "Barum"}
+        ]
+        this._products = [
+            {
+                id: 1,
+                name: "Nordman SX3",
+                price: 5000,
+                rating: 5,
+                img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'
+            },
+            {
+                id: 2,
+                name: "Hakka Blue 3",
+                price: 6000,
+                rating: 5,
+                img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'
+            },
+            {
+                id: 3,
+                name: "Brillantis 2",
+                price: 7000,
+                rating: 5,
+                img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'
+            },
+            {
+                id: 4,
+                name: "Mobile",
+                price: 5500,
+                rating: 5,
+                img: 'https://img.exist.ru/img.jpg?Key=c558cbf6-cd2b-489c-8d2d-e26c1b8ceb04_304&Size=1600x1400&MethodType=36'
+            }
+        ]
+        makeAutoObservable(this)
+    }
+
+    setTypes(types) {
+        this._types = types
+    }
+
+    setBrand(brand) {
+        this._brands = brand
+    }
+
+    setDevice(products) {
+        this._products = products
+    }
+
+    get types() {
+        return this._types
+    }
+
+    get brands() {
+        return this._brands
+    }
+
+    get products() {
+        return this._products
+    }
+}
