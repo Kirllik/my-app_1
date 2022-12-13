7595abc1505defc40bbb326cb4bfdfbb8828c26f
Kirllik <krejg007@rambler.ru>
2022-12-13 12:10:11 +0300

    Вариант NavBar c module.css не адаптивный.

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React, {useContext} from 'react';
+import {NavLink} from "react-router-dom";
+import {Context} from "../index";
+import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import {SHOP_ROUTE} from "../utils/consts";
+import s from "../CSS/NavNar.module.css"
+import {observer} from "mobx-react-lite";
+
+const NavBar = observer(() => {
+    const {user} = useContext(Context)
+    return (
+        <Navbar bg="dark" variant="dark">
+          {/*  <Container>*/}
+                <NavLink to={SHOP_ROUTE} className={s.brand}>Auto_Components</NavLink>
+                {/*<Navbar.Brand  href='/' className={s.brand}>Auto Сomponents</Navbar.Brand>*/}
+
+                <Form className={s.form}>
+                    <FormControl type="text" placeholder="Search"/>
+                    <Button variant="outline-info" className={s.button_search}>Search</Button>
+                </Form>
+                {user.isAuth ?
+                    <Nav className={s.nav}>
+                        <Button variant="outline-info">Админ панель</Button>
+                        <Button variant="outline-info" className={s.button_1}>Войти</Button>
+                        {/*<Nav.Link href='/'>Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+                    </Nav>
+                    :
+                    <Nav className={s.nav}>
+                        <Button
+                            variant="outline-info"
+                            className={s.button_2}
+                            onClick={() => user.setIsAuth(true)}>Авторизация
+                        </Button>
+                    </Nav>
+                }
+            {/*</Container>*/}
+        </Navbar>
+    );
+});
+
+export default NavBar;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -11,34 +11,10 @@ export default class DeviceStore {
             {id: 1, name: "Barum"}
         ]
         this._products = [
-            {
-                id: 1,
-                name: "Nordman SX3",
-                price: 5000,
-                rating: 5,
-                img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'
-            },
-            {
-                id: 2,
-                name: "Hakka Blue 3",
-                price: 6000,
-                rating: 5,
-                img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'
-            },
-            {
-                id: 3,
-                name: "Brillantis 2",
-                price: 7000,
-                rating: 5,
-                img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'
-            },
-            {
-                id: 4,
-                name: "Mobile",
-                price: 5500,
-                rating: 5,
-                img: 'https://img.exist.ru/img.jpg?Key=c558cbf6-cd2b-489c-8d2d-e26c1b8ceb04_304&Size=1600x1400&MethodType=36'
-            }
+            {id: 1, name: "Nordman SX3", price: 5000, rating: 5, img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'},
+            {id: 2, name: "Hakka Blue 3", price: 6000, rating: 5, img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'},
+            {id: 3, name: "Brillantis 2", price: 7000, rating: 5, img: 'https://img.exist.ru/img.jpg?Key=7981c580-72e7-47ea-bd14-35239db4029d_304&Size=1600x1400&MethodType=36'},
+            {id: 4, name: "Mobile", price: 5500, rating: 5, img: 'https://img.exist.ru/img.jpg?Key=c558cbf6-cd2b-489c-8d2d-e26c1b8ceb04_304&Size=1600x1400&MethodType=36'}
         ]
         makeAutoObservable(this)
     }
