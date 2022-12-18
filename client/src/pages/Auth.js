9ba93c3ed3663d622c6da6ae8484ef98ffa3dc6d
Kirllik <krejg007@rambler.ru>
2022-12-18 23:06:30 +0300

    Форма регистрации и авторизации на странице /auth and /registration + КОММЕНТЫ

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {Button, Card, Container, Form, FormControl, Row} from "react-bootstrap";
+import {Button, Card, Container, Form} from "react-bootstrap";
 import {AUTH_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 import {NavLink, useLocation} from "react-router-dom";
 
 const Auth = () => {
-    const location = useLocation()
+    const location = useLocation() //Для выуживания pathname из строки с URL
     const isLogin = location.pathname === AUTH_ROUTE
-    console.log(isLogin);
+    console.log(location, isLogin);
 
     return (
         <Container
             className="d-flex justify-content-center align-items-center "
-            style={{height: window.innerHeight - 54}}
+            style={{height: window.innerHeight - 54}}  /*ЦЕНТР = высота всего браузера - высота NawBar*/
         >
-            <Card style={{width: 600}} className="p-5">
+            <Card style={{width: 600}} className="p-3">
                 <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control
@@ -24,7 +24,6 @@ const Auth = () => {
                         className="mt-3"
                         placeholder="Введите ваш пароль..."
                     />
-
                     <div className="d-flex justify-content-between mt-3">
                         {isLogin ?
                             <div>
@@ -38,7 +37,7 @@ const Auth = () => {
                         <Button
                             variant={"outline-info"}
                         >
-                            {isLogin ? "Войти" : "Регистрация" }
+                            {isLogin ? "Войти" : "Регистрация"}
                         </Button>
                     </div>
                 </Form>
